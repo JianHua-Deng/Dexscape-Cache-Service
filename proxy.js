bb4ea28f5a4019db91facd5626278072ae7e35d5
JianHua-Deng <metails1203@gmail.com>
2024-07-27 16:18:20 -0400

    added proxy server

---
+++ proxy.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import cors from 'cors'
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: './.env'})
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+    console.log('Incoming request:', req.method, req.url);
+    next();
+});
+
+
+
+const mangaCoversProxy = createProxyMiddleware({
+    target: 'https://uploads.mangadex.org/covers/',
+    changeOrigin: true,
+    pathRewrite: {
+        "^/covers": "/covers",
+    },
+    logLevel: 'debug',
+    logger: console,
+    onProxyReq: (proxyReq, req, res) => {
+        console.log('Proxying request:' + req.url);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        //proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        console.log('Received response for:' + req.url);
+    },
+
+});
+
+const mangaSearchProxy = createProxyMiddleware({
+    target: 'https://api.mangadex.org/manga',
+    changeOrigin: true,
+    pathRewrite: {
+        "^/manga": "/manga",
+    },
+    logLevel: 'debug',
+    logger: console,
+    onProxyReq: (proxyReq, req, res) => {
+        console.log('Proxying request:' + req.url);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        //proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        console.log('Received response for:' + req.url);
+    },
+
+})
+
+app.use('/manga', mangaSearchProxy);
+app.use('/covers', mangaCoversProxy);
+
+
+app.listen(PORT, () => {
+    console.log("It is currently running on PORT: " + PORT);
+})
