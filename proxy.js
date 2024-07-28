c37adaeba6334a56085392dc4350a37a16db8b95
JianHua-Deng <metails1203@gmail.com>
2024-07-28 17:34:35 -0400

    edited package json

--- proxy.js
+++ proxy.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import createProxyMiddleware from 'http-proxy-middleware';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import path from 'path';
-
 dotenv.config({path: './.env'})
 
 const PORT = process.env.PORT || 3000;
@@ -12,26 +11,6 @@ const app = express();
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
-app.use((req, res, next) => {
-    console.log('Incoming request:', req.method, req.url);
-    next();
-});
-
-app.use("/covers", (req, res, next) => {
-    req.headers = { "user-agent": "Mangasite/1.0.0" };
-    next(); 
-  });
-
-  app.use("/manga", (req, res, next) => {
-    req.headers = { "user-agent": "Mangasite/1.0.0" };
-    next(); 
-  });
-
-app.use(cors());
-
-app.use(morgan('dev'));
 
 const mangaCoversProxy = createProxyMiddleware({
     target: 'https://uploads.mangadex.org/covers/',
@@ -47,7 +26,28 @@ const mangaSearchProxy = createProxyMiddleware({
     logLevel: 'debug',
     logger: console,
 
-})
+});
+
+
+app.use((req, res, next) => {
+    console.log('Incoming request:', req.method, req.url);
+    next();
+});
+
+//removing headers except user-agent, didn't work if I don't include user-agent
+app.use("/covers", (req, res, next) => {
+    req.headers = { "user-agent": "Mangasite/1.0.0" };
+    next(); 
+  });
+
+  app.use("/manga", (req, res, next) => {
+    req.headers = { "user-agent": "Mangasite/1.0.0" };
+    next(); 
+  });
+
+app.use(cors());
+
+app.use(morgan('dev'));
 
 app.use('/manga', mangaSearchProxy);
 app.use('/covers', mangaCoversProxy);
