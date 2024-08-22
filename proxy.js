4f43158c81113f392acfbe5f741a0ebddc9f9314
JianHua-Deng <metails1203@gmail.com>
2024-08-22 00:14:29 -0400

    added chapter fetching and dynamic fetching chapter image

--- proxy.js
+++ proxy.js
@@ -8,6 +8,7 @@ dotenv.config({path: './.env'})
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -21,10 +22,10 @@ app.use("/covers", (req, res, next) => {
     next(); 
   });
 
-  app.use("/manga", (req, res, next) => {
-    req.headers = { "user-agent": "Mangasite/1.0.0" };
-    next(); 
-  });
+app.use("/manga", (req, res, next) => {
+  req.headers = { "user-agent": "Mangasite/1.0.0" };
+  next(); 
+});
 
 
 const mangaCoversProxy = createProxyMiddleware({
@@ -43,23 +44,42 @@ const mangaSearchProxy = createProxyMiddleware({
 
 });
 
-
-app.use((req, res, next) => {
-    console.log('Incoming request:', req.method, req.url);
-    next();
+const chapterMetaDataProxy = createProxyMiddleware({
+  target: 'https://api.mangadex.org/at-home/',
+  changeOrigin: true,
+  logLevel: 'debug',
+  logger: console,
 });
 
-app.use(cors());
+const chapterImageProxy = createProxyMiddleware({
+  target: 'https://uploads.mangadex.org',
+  changeOrigin: true,
+  logLevel: 'debug',
+  logger: console,
+  router: (req) => {
+    const baseUrl = req.url.match(/(?<=https?:\/\/)[^\/]+(?=\/data)/); // Matching the baseUrl and only the baseUrl from the request
+    if (baseUrl){
+      //console.log(`Original URL: ${req.url}, BaseUrl: ${baseUrl}`);
+      return baseUrl.includes('https://') ? `${baseUrl}` : `https://${baseUrl}`;
+    }
+    return 'https://uploads.mangadex.org';
+  },
+  pathRewrite: (path, req) => {
+    // Empty out anything before /data, and take that as the path
+    return path.replace(/^.*(?=\/data)/, "");
+  },
 
-app.use(morgan('dev'));
+});
 
 app.use('/manga', mangaSearchProxy);
 app.use('/covers', mangaCoversProxy);
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/at-home', chapterMetaDataProxy);
+app.use('/chapter-image', chapterImageProxy);
 
 
 app.listen(PORT, () => {
     console.log("It is currently running on PORT: " + PORT);
 })
+
+//const __dirname = path.resolve();
+//app.use(express.static(path.join(__dirname, 'dist')));
