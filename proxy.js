e1bb3baa157beabb2e0d7004c275b497ddc36254
JianHua-Deng <metails1203@gmail.com>
2024-08-22 12:02:44 -0400

    logging headers

--- proxy.js
+++ proxy.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    console.log("Res Headers: " + res.headers);
+    console.log("Res Headers: " + res.getHeaders());
     next();
 });
 
